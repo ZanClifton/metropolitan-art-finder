4c6652e8146c2b74b5ec941848954f2a7a10cf13
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-04-11 12:02:57 +0100

    deleted files

---
+++ metropolitan-art-finder/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Metropolitan Museum Art Search
+      </header>
+    </div>
+  );
+}
+
+export default App;
